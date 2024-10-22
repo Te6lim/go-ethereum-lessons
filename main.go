d1f29735f48e9dea8aa8f0cf18e9337df7211752
te6lim <te6lim@gmail.com>
2024-10-22 21:54:31 +0100

    smart contract compilation and abi

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"math/big"
 	"os"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -17,6 +18,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/te6lim/go-ethereum-lessons/contracts/store"
 )
 
 func main() {
@@ -53,6 +55,8 @@ func main() {
 	//CreateRawTransaction(*client)
 
 	//SendRawTransactionData(*client)
+
+	DeployingASmartContract(*client)
 
 }
 
@@ -397,4 +401,77 @@ func SendRawTransactionData(client ethclient.Client) {
 	}
 
 	fmt.Printf("tx sent: %s\n", tx.Hash().Hex())
+}
+
+func SmartContractComilationAndABI() {
+	// install solc
+	// install abigen (for genetating an ABI)
+	// solc --bin <path to smart contract> -o to build the sol file
+	// run solc --abi <file path> -o build to generate abi
+	// run abigen --abi=<abi file path> --pkg=<package name> --out=<output file path> to convert abi file into go file
+
+	// in order to deploy the contract, we also need to cmpile the solidity smart contact to EVM bytecode. the bin file is required fo generating the deploy methods on the go contract file
+	// solc --bin <smart contract file path> -o build
+	//abigen --bin=<path to store new bin file> --abi=<path to abi file> --pkg=<package>  --out=<path to store go file>
+}
+
+func DeployingASmartContract(client ethclient.Client) {
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chainId, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(300000)
+	auth.GasPrice = gasPrice
+
+	input := "1.0"
+
+	address, tx, insance, err := store.DeployStore(auth, &client, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("address: %v\n", address)
+	fmt.Printf("transaction hex: %v\n", tx.Hash().Hex())
+	_ = insance
+}
+
+func LoadingASmartContract(client ethclient.Client) {
+	address := common.HexToAddress("0x147B8eb97fD247D06C4006D269c90C1908Fb5D54")
+	instance, err := store.NewStore(address, &client)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = instance
 }
